20bef690c2e9ee5408ef40a26052b4258967ac7e
narendra28051991 <narendra28051991@gmail.com>
2023-09-23 09:55:44 +0200

    App routes & index file updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,43 @@
 import React from "react";
+// firebase imports
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-  Navigate,
+  Navigate
 } from "react-router-dom";
+// pages
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import Password from "./pages/password/Password";
 import Dashboard from "./pages/dashboard/Dashboard";
+// hooks
+import { useAuthContext } from "./hooks/useAuthContext";
+// styles
 import "./App.css";
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <React.Fragment>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/password" element={<Password />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/" element={<Navigate to="/login" replace />} />
-    </React.Fragment>
-  )
-);
-
 function App(): JSX.Element {
+  const { user } = useAuthContext();
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <React.Fragment>
+        <Route
+          path="/"
+          element={user ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />}
+        />
+        <Route path="/signup" element={user ? <Navigate to="/dashboard" replace /> : <Signup />} />
+        <Route path="/login" element={user ? <Navigate to="/dashboard" replace /> : <Login />} />
+        <Route
+          path="/password"
+          element={user ? <Navigate to="/dashboard" replace /> : <Password />}
+        />
+        <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" replace />} />
+      </React.Fragment>
+    )
+  );
+
   return <RouterProvider router={router} />;
 }
 
