7ef0dcb5ddc258cfd2c9bd7c0f6e3f3e24236191
narendra28051991 <narendra28051991@gmail.com>
2023-09-23 17:50:35 +0200

    updated the routing with a function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,60 @@
-import React from "react";
-// firebase imports
+import { ReactNode } from "react";
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
   RouterProvider,
   Navigate
 } from "react-router-dom";
-// pages
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import Password from "./pages/password/Password";
 import Dashboard from "./pages/dashboard/Dashboard";
-// hooks
 import { useAuthContext } from "./hooks/useAuthContext";
-// styles
 import "./App.css";
 
 function App(): JSX.Element {
   const { user } = useAuthContext();
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <React.Fragment>
-        <Route
-          path="/"
-          element={user ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />}
-        />
-        <Route path="/signup" element={user ? <Navigate to="/dashboard" replace /> : <Signup />} />
-        <Route path="/login" element={user ? <Navigate to="/dashboard" replace /> : <Login />} />
-        <Route
-          path="/password"
-          element={user ? <Navigate to="/dashboard" replace /> : <Password />}
-        />
-        <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" replace />} />
-      </React.Fragment>
-    )
-  );
+  const renderPage = ( authenticated: ReactNode, notAuthenticated: ReactNode ): ReactNode => {
+    return user ? authenticated : notAuthenticated;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: renderPage(
+        <Navigate to="/dashboard" replace />,
+        <Navigate to="/login" replace />
+      )
+    },
+    {
+      path: "/signup",
+      element: renderPage(
+        <Navigate to="/dashboard" replace />,
+        <Signup />
+      )
+    },
+    {
+      path: "/login",
+      element: renderPage(
+        <Navigate to="/dashboard" replace />,
+        <Login />
+      )
+    },
+    {
+      path: "/password",
+      element: renderPage(
+        <Navigate to="/dashboard" replace />,
+        <Password />
+      )
+    },
+    {
+      path: "/dashboard",
+      element: renderPage(
+        <Dashboard />,
+        <Navigate to="/login" replace />
+      )
+    },
+  ]);
 
   return <RouterProvider router={router} />;
 }
