8572c98aff13e758af73e9cdba4de124fedbbafa
narendra28051991 <narendra28051991@gmail.com>
2023-09-24 22:00:01 +0200

    deleted error store

--- frontend/src/store/error.store.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-import { ErrorState } from "../types";
-
-const useErrorState = create<ErrorState>((set) => ({
-  error: null,
-  setError: (error) => set({ error }),
-}));
-
-export default useErrorState;
