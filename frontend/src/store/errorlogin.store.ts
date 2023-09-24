15889a89cd2f05b96a669368d42de5c6a96a66b4
narendra28051991 <narendra28051991@gmail.com>
2023-09-24 22:01:35 +0200

    provided different states for login and signup

---
+++ frontend/src/store/errorlogin.store.ts
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+import { ErrorState } from "../types";
+
+const useErrorLoginState = create<ErrorState>((set) => ({
+  error: null,
+  setError: (error) => set({ error }),
+}));
+
+export default useErrorLoginState;
---
+++ frontend/src/store/errorsignup.store.ts
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+import { ErrorState } from "../types";
+
+const useErrorSignupState = create<ErrorState>((set) => ({
+  error: null,
+  setError: (error) => set({ error }),
+}));
+
+export default useErrorSignupState;
