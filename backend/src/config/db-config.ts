3eafcd44d4aa5edf960d795a3e7e909540bd00d1
Samima Hassan <samimahassan55@gmail.com>
2023-09-26 21:15:54 +0200

    register routed added

--- backend/src/config/db-config.ts
+++ backend/src/config/db-config.ts
@@ -2,5 +2,7 @@ import { config } from '../knexfile';
 import knex from 'knex';
 const currentEnv: string = process.env.ENVIRONMENT || 'development';
 const db = knex(config[currentEnv]);
-
+db.raw("SELECT VERSION()").then(() => {
+    console.log(`connection to db successful!`);
+  });
 export default db;
--- backend/src/controllers/registration/index.ts
+++ backend/src/controllers/registration/index.ts
@@ -2,16 +2,31 @@ import { Request, Response } from "express";
 import { adminFireAuth } from "../../firebase/config";
 import db from "../../config/db-config";
 
-export const register= async (req: Request, res: Response) => {
-   try{
-    const {first_name,last_name, email,user_type} = req.body;
-       // Store user data in PostgreSQL using Knex.js
-       console.log(req.body);
-      await db.select('users').insert({first_name,last_name,email,user_type});
-      res.status(201).json({ message: 'User registered successfully' });
-   }
-   catch(error){
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { first_name, last_name, email, user_type } = req.body;
+   
+    console.log(req.body);
+    const newUser = await db("users").insert({
+      first_name,
+      last_name,
+      email,
+      user_type,
+    });
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-   }
-}
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+
+ 
+
+ 
+
+ 
+ 
+ 
+ 
+ 
