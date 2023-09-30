0e604248a9d5b55b48784c079b73360bf5f2b984
Nelia11 <levit.nelia@icloud.com>
2023-09-30 10:27:41 +0200

    Remove unused routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,4 @@
 import express, { Router } from 'express';
-import auth from './auth';
 import registration from'./registration';
 import course from './course';
 import lesson from './lesson';
@@ -8,7 +7,6 @@ import benefits from './benefits';
 
 const router: Router = express.Router();
 
-router.use('/auth', auth);
 router.use('/user', registration)
 router.use('/courses', course);
 router.use('/course', lesson);
