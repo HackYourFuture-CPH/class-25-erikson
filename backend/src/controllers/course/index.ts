8203f83bb08b9903379a0c3c7f534353aa1accf5
Nelia11 <levit.nelia@icloud.com>
2023-10-02 11:29:44 +0200

    Add POST method to create a new course

--- backend/src/controllers/course/index.ts
+++ backend/src/controllers/course/index.ts
@@ -25,7 +25,87 @@ const getCourseById = async(req: Request, res: Response) => {
   }
 }
 
+const addNewCourse = async(req: Request, res: Response) => {
+  try {
+    const mentorId = req.params.id;
+
+    const { 
+      course_title, 
+      course_description, 
+      category, 
+      price, 
+      price_type, 
+      course_img, 
+      sale_img,
+      key_learning,
+      lessons,
+      faqs,
+      benefits
+    } = req.body;
+
+    await db.transaction(async (trx) => {
+      const [ courseId ] = await trx('course')
+        .insert({
+          course_title, 
+          course_description, 
+          category, 
+          price,
+          price_type,
+          course_img, 
+          sale_img,
+          key_learning,
+          mentor: mentorId
+        })
+        .returning('id');
+      
+        for (const lesson of lessons) {
+          const [lessonId] = await trx('lesson')
+            .insert({
+              lesson_title: lesson.lesson_title,
+              lesson_description: lesson.lesson_description,
+              lesson_img: lesson.lesson_img,
+              course_id: courseId.id,
+            })
+            .returning('id');
+
+          for (const resource of lesson.resources) {
+            await trx('resource')
+            .insert({
+              resource_url: resource.resource_url,
+              lesson_id: lessonId.id
+            })
+          }
+        }  
+
+        for (const faq of faqs) {
+          await trx('faq')
+          .insert({
+            faq: faq.faq,
+            answer: faq.answer,
+            course_id: courseId.id
+          });
+        }
+
+        for(const benefit of benefits) {
+          await trx('benefit')
+          .insert({
+            benefit_individual: benefit.benefit_individual,
+            benefit_monthly: benefit.benefit_monthly,
+            course_id: courseId.id
+          })
+        }
+
+      await trx.commit();
+
+      return res.status(201).json({ message: "The course has been created!" })
+    })
+  } catch (error) {
+    res.status(500).json({ error: `Can't add course: ${error}` });    
+  }
+}
+
 export {
   getAll,
-  getCourseById
+  getCourseById, 
+  addNewCourse
 }
--- backend/src/routes/course/index.ts
+++ backend/src/routes/course/index.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
-import { getAll, getCourseById } from '../../controllers/course';
+import { getAll, getCourseById, addNewCourse } from '../../controllers/course';
 
 const course = Router();
 
 course.get('/all', getAll);
-course.get('/:id', getCourseById);
+course.get('/course/:id', getCourseById); // id - course id
+course.post('/:id/add_course', addNewCourse); //id - mentor id
 
 export default course;
