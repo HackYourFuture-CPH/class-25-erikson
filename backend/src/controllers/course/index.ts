96726cc842c7ac314133313f7da5c6bd94ec72e5
Nelia11 <levit.nelia@icloud.com>
2023-10-04 12:45:54 +0200

    Update .gitignore; Update course routes

--- backend/src/controllers/course/index.ts
+++ backend/src/controllers/course/index.ts
@@ -3,7 +3,13 @@ import db from '../../config/db-config';
 
 const getAll = async(req: Request, res: Response) => {
   try {
-    const courses = await db('course').select('title', 'description', 'category', 'img_course');
+    const courses = await db('course')
+    .select(
+      'id',
+      'course_title', 
+      'course_category', 
+      'course_image',
+    )
     courses.length === 0 
     ? res.status(404).json({message: 'No courses'})
     : res.status(200).json(courses);
@@ -15,10 +21,17 @@ const getAll = async(req: Request, res: Response) => {
 const getCourseById = async(req: Request, res: Response) => {
   try {
     const courseId = req.params.id;
-    const course = await db('course').select('title', 'description', 'category', 'img_course').where({'id' : courseId}).first();
-    if (!course) {
-      return res.status(404).json({ message: 'Selected course not found' });
-    }
+    const course = await db('course')
+    .select(
+      'course_title', 
+      'course_description', 
+      'course_category', 
+      'course_image',
+      'lesson_title',
+      'lesson_resources')
+    .join('lesson', {'lesson.course_id': 'course.id'})
+    .join('resource', {'lesson_id' : 'lesson.id'})
+    .where({'course.id' : courseId});
     res.status(200).json(course);
   } catch (error) {
     res.status(500).json({ error: `Can't fetch course: ${error}` });
@@ -32,63 +45,68 @@ const addNewCourse = async(req: Request, res: Response) => {
     const { 
       course_title, 
       course_description, 
-      course_category,
-      course_image,
-      subscription_type,
+      course_category, 
       course_price,
-      lesson_title, 
-      lesson_image,
-      lesson_description,
-      lesson_resources, 
+      subscription_type,
+      course_image, 
       sales_image,
-      faq,
-      faq_answer,
       key_learning,
-      pricing_benefits
+      lessons,
+      faqs,
+      benefits
     } = req.body;
 
-    await db.transaction(async (trx) => {
-      const [ courseId ] = await trx('course')
-        .insert({
-          course_title, 
-          course_description, 
-          course_category, 
-          course_image,
-          subscription_type,
-          course_price, 
-          sales_image,
-          key_learning,
-          mentor: mentorId
-        })
-        .returning('id');
-      
-        const [lessonId] = await trx('lesson')
-        .insert({
-          lesson_title,
-          lesson_image,
-          lesson_description,
-          course_id: courseId.id,
-        })
-        .returning('id');
-
-        await trx('resource')
-        .insert({
-          lesson_resources,
-          lesson_id: lessonId.id
-        })
-        
-        await trx('faq')
-        .insert({
-          faq,
-          faq_answer,
-          course_id: courseId.id
-        });
-
-        await trx('benefit')
-        .insert({
-          pricing_benefits,
-          course_id: courseId.id
-        })
+      await db.transaction(async (trx) => {
+        const [ courseId ] = await trx('course')
+          .insert({
+            course_title, 
+            course_description, 
+            course_category, 
+            course_price,
+            subscription_type,
+            course_image, 
+            sales_image,
+            key_learning,
+            mentor: mentorId
+          })
+          .returning('id');
+  
+          for (const lesson of lessons) {
+            const [lessonId] = await trx('lesson')
+              .insert({
+                lesson_title: lesson.lesson_title,
+                lesson_description: lesson.lesson_description,
+                lesson_image: lesson.lesson_image,
+                course_id: courseId.id,
+              })
+              .returning('id');
+  
+            for (const resource of lesson.resources) {
+              await trx('resource')
+              .insert({
+                lesson_resources: resource.lesson_resources,
+                lesson_id: lessonId.id
+              })
+            }
+          }  
+  
+          for (const faq of faqs) {
+            await trx('faq')
+            .insert({
+              faq: faq.faq,
+              faq_answer: faq.faq_answer,
+              course_id: courseId.id
+            });
+          }
+  
+          for(const benefit of benefits) {
+            await trx('benefit')
+            .insert({
+              benefit_individual: benefit.benefit_individual,
+              benefit_monthly: benefit.benefit_monthly,
+              course_id: courseId.id
+            })
+          }
 
       await trx.commit();
 
