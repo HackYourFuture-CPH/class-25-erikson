b2c65fe19cce2c0ecbb455f4d42a7d1b01a66cea
Samima Hassan <samimahassan55@gmail.com>
2023-10-04 20:28:54 +0200

    styles added for lessonForm

--- frontend/src/components/formDetails/lesson/LessonForm.tsx
+++ frontend/src/components/formDetails/lesson/LessonForm.tsx
@@ -1,6 +1,6 @@
 import { FileDrop } from '../FileDrop/FileDrop';
 import FormWrapper from '../wrapper/FormWrapper';
-
+import styles from './LessonForm.module.css';
 type LessonData = {
   lesson_title: string;
   lesson_image: File;
@@ -24,30 +24,44 @@ const LessonForm = ({
   };
   return (
     <FormWrapper title='Lesson'>
-      <FileDrop onImageSelect={handleImageChange} />
-      <label>title</label>
-      <input
-        required
-        type='text'
-        value={lesson_title}
-        onChange={(e) => updateFields({ lesson_title: e.target.value })}
-      />
+      <div className={styles.container}>
+        <div className={styles.titleContainer}>
+          <label>Lesson Title</label>
+          <input
+            className={styles.titleInput}
+            required
+            type='text'
+            placeholder='Set course title'
+            value={lesson_title}
+            onChange={(e) => updateFields({ lesson_title: e.target.value })}
+          />
+        </div>
+        <FileDrop onImageSelect={handleImageChange} />
 
-      <label>lessson description</label>
-      <input
-        required
-        type='text'
-        value={lesson_description}
-        onChange={(e) => updateFields({ lesson_description: e.target.value })}
-      />
+        <div className={styles.descriptionContainer}>
+          <label>lessson description</label>
+          <input
+            className={styles.descriptionInput}
+            required
+            type='text'
+            placeholder='Input Text Here'
+            value={lesson_description}
+            onChange={(e) => updateFields({ lesson_description: e.target.value })}
+          />
+        </div>
 
-      <label>Lesson Resources</label>
-      <input
-        required
-        type='text'
-        value={lesson_resources}
-        onChange={(e) => updateFields({ lesson_resources: e.target.value })}
-      />
+        <div className={styles.resourcesContainer}>
+          <label>Lesson Resources</label>
+          <input
+            className={styles.resourcesInput}
+            required
+            type='text'
+            placeholder='Resources Here'
+            value={lesson_resources}
+            onChange={(e) => updateFields({ lesson_resources: e.target.value })}
+          />
+        </div>
+      </div>
     </FormWrapper>
   );
 };
