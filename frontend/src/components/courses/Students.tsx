f755475d1a8450b4bceccc5568bb2c0f0d22aca7
narendra28051991 <narendra28051991@gmail.com>
2023-10-06 11:58:08 +0200

    fetching a single user instead of mock data

--- frontend/src/components/courses/Students.tsx
+++ frontend/src/components/courses/Students.tsx
@@ -1,11 +1,56 @@
 import React from 'react';
+// import { useEffect, useState } from 'react';
 import Filters from './Filters';
 import CourseList from './CourseList';
 import { Link } from 'react-router-dom';
+// import { useAuthContext } from '../../hooks/useAuthContext';
+// import axios from 'axios';
 import { users } from '../../data/data';
 
 const StudentDashboard: React.FC = () => {
   const userType = users[1].type;
+  // const { user } = useAuthContext();
+  // const [currentUser, setCurrentUser] = useState(null);
+
+  // useEffect(() => {
+  //   let isMounted = true;
+
+  //   const fetchData = async () => {
+  //     try {
+  //       if (user?.emailVerified) {
+  //         const idToken = await user.getIdToken();
+
+  //         const response = await axios.get(`/users/${user.uid}`, {
+  //           headers: {
+  //             Authorization: `Bearer ${idToken}`
+  //           }
+  //         });
+
+  //         if (isMounted) {
+  //           setCurrentUser(response.data);
+  //         }
+  //       } else {
+  //         if (isMounted) {
+  //           setCurrentUser(null);
+  //         }
+  //       }
+  //     } catch (error) {
+  //       console.error('Error fetching currentUser data:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+
+  //   return () => {
+  //     isMounted = false;
+  //   };
+  // }, [user]);
+
+  // if (!currentUser) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // const userType = currentUser.type;
 
   return (
     <div>
