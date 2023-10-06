1c685b20711ee66c83e69059745b1a6b126df4b8
narendra28051991 <narendra28051991@gmail.com>
2023-10-07 00:37:45 +0200

    modified all courses styles

--- frontend/src/components/courses/Filters.tsx
+++ frontend/src/components/courses/Filters.tsx
@@ -1,16 +1,17 @@
-import useFilterStore from "../../store/filter.store";
-import "./Filters.css";
+import useFilterStore from '../../store/filter.store';
+import styles from './Filters.module.css';
+
 const Filters: React.FC = () => {
   const { selectedFilter, setSelectedFilter } = useFilterStore();
 
   const filterButtons = ['All', 'Personal', 'Finance', 'Professional', 'Live Event'];
 
   return (
-    <div className="filters">
+    <div className={styles.filters}>
       {filterButtons.map((filter) => (
         <button
           key={filter}
-          className={selectedFilter === filter ? 'active' : ''} 
+          className={selectedFilter === filter ? styles.active : ''}
           onClick={() => setSelectedFilter(filter)}
         >
           {filter}
