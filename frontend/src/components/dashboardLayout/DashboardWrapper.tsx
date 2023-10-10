e1772de0497763078b716c45d56187b0a12ce3a7
Samima Hassan <samimahassan55@gmail.com>
2023-10-10 21:49:34 +0200

    side bar stikcy

--- frontend/src/components/dashboardLayout/DashboardWrapper.tsx
+++ frontend/src/components/dashboardLayout/DashboardWrapper.tsx
@@ -30,70 +30,70 @@ const DashboardWrapper = ({ children }: FormWrapperProps) => {
 
   const handleArrow = (): void => {
     const logout = document.querySelector('.dropdown-menu');
-    const arrow = document.querySelector('.handleArrow')!
+    const arrow = document.querySelector('.handleArrow')!;
 
     logout?.classList.toggle('active');
     arrow.innerHTML = logout?.classList.contains('active') ? '▲' : '▼';
-
-    
   };
 
   return (
     <>
       <div className='dashboard-layout'>
         <nav className={`sidebar ${isSidebarOpen ? 'open' : 'hidden'}`}>
-          <div className='dashboard-logo'>
-            <img src='images/dashboard.svg' alt='dashboard' />
-          </div>
-
-          <div className='sidebar-menu'>
-            <div className='sidebar-item'>
-              <div>
-                <img src='images/dashboard2.svg' alt='dashboard2' />
-                <p>Dashboard</p>
-              </div>
+          <div className='sticky'>
+            <div className='dashboard-logo'>
+              <img src='images/dashboard.svg' alt='dashboard' />
             </div>
 
-            <div className='sidebar-item'>
-              <div>
-                <img src='images/goals.svg' alt='dashboard2' />
-                <p>Goals</p>
+            <div className='sidebar-menu'>
+              <div className='sidebar-item'>
+                <div>
+                  <img src='images/dashboard2.svg' alt='dashboard2' />
+                  <p>Dashboard</p>
+                </div>
               </div>
-            </div>
 
-            <div className='sidebar-item'>
-              <div>
-                <img src='images/video.svg' alt='live schedule' />
-                <p>Live Schedule</p>
+              <div className='sidebar-item'>
+                <div>
+                  <img src='images/goals.svg' alt='dashboard2' />
+                  <p>Goals</p>
+                </div>
               </div>
-            </div>
 
-            <div className='sidebar-item'>
-              <div>
-                <img src='images/calendar.svg' alt='class schedule' />
-                <p>Class Schedule</p>
+              <div className='sidebar-item'>
+                <div>
+                  <img src='images/video.svg' alt='live schedule' />
+                  <p>Live Schedule</p>
+                </div>
               </div>
-            </div>
 
-            <div className='sidebar-item'>
-              <div>
-                <img src='images/bookmark.svg' alt='course' />
-                <p>Course</p>
+              <div className='sidebar-item'>
+                <div>
+                  <img src='images/calendar.svg' alt='class schedule' />
+                  <p>Class Schedule</p>
+                </div>
               </div>
-            </div>
 
-            <div className='sidebar-item'>
-              <div>
-                <img src='images/document.svg' alt='resources' />
-
-                <p>Resources</p>
+              <div className='sidebar-item'>
+                <div>
+                  <img src='images/bookmark.svg' alt='course' />
+                  <p>Course</p>
+                </div>
               </div>
-            </div>
 
-            <div className='sidebar-item'>
-              <div>
-                <img src='images/folder.svg' alt='directory' />
-                <p>Directory</p>
+              <div className='sidebar-item'>
+                <div>
+                  <img src='images/document.svg' alt='resources' />
+
+                  <p>Resources</p>
+                </div>
+              </div>
+
+              <div className='sidebar-item'>
+                <div>
+                  <img src='images/folder.svg' alt='directory' />
+                  <p>Directory</p>
+                </div>
               </div>
             </div>
           </div>
@@ -116,7 +116,10 @@ const DashboardWrapper = ({ children }: FormWrapperProps) => {
                 <div className='user-info'>
                   <img className='user-img' src={Person} alt='user-display' />
                   <p>
-                    {user?.displayName} <span className='handleArrow' onClick={handleArrow}>▼</span>
+                    {user?.displayName}{' '}
+                    <span className='handleArrow' onClick={handleArrow}>
+                      ▼
+                    </span>
                   </p>
                 </div>
                 <div className='dropdown-menu'>
