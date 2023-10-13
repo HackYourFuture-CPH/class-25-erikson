170b42034e71f78f06d9ebe57b531b723221e931
Nelia11 <levit.nelia@icloud.com>
2023-10-13 13:41:42 +0200

    Fetch current user using hook in Dashboard.tsx

--- frontend/src/components/courses/Students.tsx
+++ frontend/src/components/courses/Students.tsx
@@ -1,57 +1,12 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useAuthContext } from '../../hooks/useAuthContext';
-import axios from 'axios';
 import Filters from './Filters';
 import CourseList from './CourseList';
 import useUserStore from '../../store/user.store';
 
 const StudentDashboard: React.FC = () => {
-  const { user } = useAuthContext();
-  const { currentUser, setCurrentUser } = useUserStore()
-
-  useEffect(() => {
-    let isMounted = true;
-
-    const fetchData = async () => {
-      try {
-        if (user?.emailVerified) {
-          const idToken = await user.getIdToken();
-
-          const response = await axios.get(`api/user/uid/${user.uid}`, {
-            headers: {
-              Authorization: `Bearer ${idToken}`
-            }
-          });
-          
-          if (isMounted) {
-            setCurrentUser(response.data);
-          }
-        }
-        else {
-          if (isMounted) {
-            setCurrentUser(null);
-          }
-        }
-      }
-      catch (error) {
-        console.error('Error fetching currentUser data:', error);
-      }
-    };
-
-    fetchData();
-
-    return () => {
-      isMounted = false;
-    };
-  }, [user, setCurrentUser]);
-
-  if (!currentUser) {
-    return <div>Loading...</div>;
-  }
-
-  const userType = currentUser.user_type;
+  const { currentUser } = useUserStore()
+  const userType = currentUser?.user_type;
 
   return (
     <div>
