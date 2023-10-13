78a1376f77f5b498cd67886e7f9c46f05eba1e0e
narendra28051991 <narendra28051991@gmail.com>
2023-10-13 10:24:10 +0200

    modified the components to use the store

--- frontend/src/components/courses/Students.tsx
+++ frontend/src/components/courses/Students.tsx
@@ -1,56 +1,61 @@
 import React from 'react';
-// import { useEffect, useState } from 'react';
-import Filters from './Filters';
-import CourseList from './CourseList';
+// import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 // import { useAuthContext } from '../../hooks/useAuthContext';
 // import axios from 'axios';
+import Filters from './Filters';
+import CourseList from './CourseList';
 import { users } from '../../data/data';
+// import useUserStore from '../../store/user.store';
 
 const StudentDashboard: React.FC = () => {
   const userType = users[1].type;
-  // const { user } = useAuthContext();
-  // const [currentUser, setCurrentUser] = useState(null);
+  /* 
+  const { user } = useAuthContext();
+  const { currentUser, setCurrentUser } = useUserStore()
 
-  // useEffect(() => {
-  //   let isMounted = true;
+  useEffect(() => {
+    let isMounted = true;
 
-  //   const fetchData = async () => {
-  //     try {
-  //       if (user?.emailVerified) {
-  //         const idToken = await user.getIdToken();
+    const fetchData = async () => {
+      try {
+        if (user?.emailVerified) {
+          const idToken = await user.getIdToken();
 
-  //         const response = await axios.get(`/users/${user.uid}`, {
-  //           headers: {
-  //             Authorization: `Bearer ${idToken}`
-  //           }
-  //         });
+          const response = await axios.get(`/user/${user.uid}`, {
+            headers: {
+              Authorization: `Bearer ${idToken}`
+            }
+          });
 
-  //         if (isMounted) {
-  //           setCurrentUser(response.data);
-  //         }
-  //       } else {
-  //         if (isMounted) {
-  //           setCurrentUser(null);
-  //         }
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching currentUser data:', error);
-  //     }
-  //   };
+          if (isMounted) {
+            setCurrentUser(response.data);
+          }
+        }
+        else {
+          if (isMounted) {
+            setCurrentUser(null);
+          }
+        }
+      }
+      catch (error) {
+        console.error('Error fetching currentUser data:', error);
+      }
+    };
 
-  //   fetchData();
+    fetchData();
 
-  //   return () => {
-  //     isMounted = false;
-  //   };
-  // }, [user]);
+    return () => {
+      isMounted = false;
+    };
+  }, [user]);
 
-  // if (!currentUser) {
-  //   return <div>Loading...</div>;
-  // }
+  if (!currentUser) {
+    return <div>Loading...</div>;
+  }
 
-  // const userType = currentUser.type;
+  const userType = currentUser.user_type;
+*/
 
   return (
     <div>
