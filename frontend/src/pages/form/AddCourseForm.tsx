23de311d0594e508274c07ca6eb5d3f4bea5d094
narendra28051991 <narendra28051991@gmail.com>
2023-10-18 09:20:59 +0200

    modified add course form

--- frontend/src/pages/form/AddCourseForm.tsx
+++ frontend/src/pages/form/AddCourseForm.tsx
@@ -1,9 +1,12 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { AddCourseFields } from '../../types/component';
 import { useMultistepForm } from '../../hooks/useMultiStepForm';
 import axios from 'axios';
+import useFirebaseStorage from '../../hooks/useFirebaseStorage';
+import useSingleCourseStore from '../../store/addSingleCourse.store';
+import useUserStore from '../../store/user.store';
 import CourseForm from '../../components/formDetails/course/CourseForm';
 import LessonForm from '../../components/formDetails/lesson/LessonForm';
 import SalesForm from '../../components/formDetails/sales/SalesForm';
@@ -11,14 +14,17 @@ import DashboardWrapper from '../../components/dashboardLayout/DashboardWrapper'
 import BackArrow from '../../assets/icons/back.svg';
 import FrontArrow from '../../assets/icons/front.svg';
 import styles from './AddCourseForm.module.css';
-import useFirebaseStorage from '../../hooks/useFirebaseStorage';
-import useSingleCourseStore from '../../store/addSingleCourse.store';
-import useUserStore from '../../store/user.store';
+import SuccessModal from './SuccessModal';
+import FailureModal from './FailureModal';
+import Loader from './Loader';
 
 const AddCourseForm: React.FC = () => {
   const { user } = useAuthContext();
   const { currentUser } = useUserStore();
   const { generateUniqueFileName, uploadImageToFirebaseStorage } = useFirebaseStorage();
+  const [isLoading, setIsLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [failure, setFailure] = useState(false);
   const navigate = useNavigate();
 
   const userType = currentUser?.user_type;
@@ -69,6 +75,7 @@ const AddCourseForm: React.FC = () => {
     if (!isLastStep) return next();
 
     try {
+      setIsLoading(true);
       // Upload course image to Firebase Storage
       const courseImagePath = `courses/${generateUniqueFileName(data.course_image.name)}`;
       const courseImageUrl = await uploadImageToFirebaseStorage(data.course_image, courseImagePath);
@@ -111,42 +118,53 @@ const AddCourseForm: React.FC = () => {
         },
       });
 
-      alert('Form Submitted');
+      setIsLoading(false);
+      setSuccess(true);
       resetForm();
       goTo(0);
       navigate('/courses');
     } catch (error) {
       console.error('Error submitting form:', error);
+      setIsLoading(false);
+      setFailure(true);
     }
   };
 
   return (
     <DashboardWrapper>
-      <div className={styles.addCourse}>
-        <form onSubmit={submitForm}>
-          {step}
-          <div className={styles.buttonsDiv}>
-            {!isFirstStep && (
-              <button type='button' onClick={back}>
-                <>
-                  <img src={BackArrow} alt='Back Arrow' />
-                  <span>Go Back</span>
-                </>
-              </button>
-            )}
-            <button type='submit' onClick={checkImageAttached}>
-              {isLastStep ? (
-                'Add Course'
-              ) : (
-                <>
-                  <span>Publish Course</span>
-                  <img src={FrontArrow} alt='Front Arrow' />
-                </>
+      {isLoading && <Loader />}
+
+      {!isLoading && (
+        <div className={styles.addCourse}>
+          <form onSubmit={submitForm}>
+            {step}
+            <div className={styles.buttonsDiv}>
+              {!isFirstStep && (
+                <button type='button' onClick={back}>
+                  <>
+                    <img src={BackArrow} alt='Back Arrow' />
+                    <span>Go Back</span>
+                  </>
+                </button>
               )}
-            </button>
-          </div>
-        </form>
-      </div>
+              <button type='submit' onClick={checkImageAttached}>
+                {isLastStep ? (
+                  'Add Course'
+                ) : (
+                  <>
+                    <span>Publish Course</span>
+                    <img src={FrontArrow} alt='Front Arrow' />
+                  </>
+                )}
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+
+      {success && <SuccessModal onClose={() => setSuccess(false)} />}
+
+      {failure && <FailureModal onClose={() => setFailure(false)} />}
     </DashboardWrapper>
   );
 };
