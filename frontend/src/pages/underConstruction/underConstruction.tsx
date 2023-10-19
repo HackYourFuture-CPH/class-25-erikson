23ed4ec0ed48bb93457ffb64bdb47a3e659ee9be
Samima Hassan <samimahassan55@gmail.com>
2023-10-19 11:42:24 +0200

    redirect for construction page is added

--- frontend/src/pages/underConstruction/underConstruction.tsx
+++ frontend/src/pages/underConstruction/underConstruction.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import styles from './underConstruction.module.css';
 const UnderConstruction: React.FC = () => {
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    const redirectTimeout = setInterval(() => {
+      if (countdown === 1) {
+        // Redirect when countdown reaches 1
+        window.location.href = '/courses'; // Replace with the actual URL of your courses page
+      } else {
+        setCountdown(countdown - 1);
+      }
+    }, 1000); // 1 second interval
+
+    return () => {
+      clearInterval(redirectTimeout); // Clear the interval if the component unmounts
+    };
+  }, [countdown]);
+
   return (
     <div className={styles.centered}>
       <h1>This page is under construction</h1>
@@ -8,6 +26,7 @@ const UnderConstruction: React.FC = () => {
         <i className={styles.icon1}>&#128679;</i>
         <i className={styles.icon2}>&#128721;</i>
       </div>
+      <p>Redirecting in {countdown} seconds...</p>
     </div>
   );
 };
